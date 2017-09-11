76c313c5dacad8931a9e899fcf197427d460fca3
TrungDT <thanhtrung.it25@gmail.com>
2017-09-11 15:06:34 +0700

    Add handle state addColor to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import { Component } from 'react'
+import { v4 } from 'uuid'
 import AddColorForm from './AddColorForm'
 import ColorList from './ColorList'
 import '../../stylesheets/APP.scss'
@@ -10,13 +11,30 @@ export default class App extends Component {
         this.state = {
             colors: []
         }
+
+        this.addColor = this.addColor.bind(this)
+    }
+
+    addColor(title, color) {
+        const colors = [
+            this.state.colors,
+            {
+                id: v4(),
+                title,
+                color,
+                rating: 0
+            }
+        ]
+        this.setState(colors)
     }
 
     render() {
+        const {addColor} = this
+        const {colors} = this.state
         return (
             <div className="app">
-                <AddColorForm />
-                <ColorList />
+                <AddColorForm onNewColor={addColor} />
+                <ColorList colors={colors} />
             </div>
         )
     }
