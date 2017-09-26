ddaf1127bd1125c70a236eabc1d87ed597dacf96
TrungDT <thanhtrung.it25@gmail.com>
2017-09-26 23:59:46 +0700

    Add actions creator function

---
+++ src/actions.js
@@ -0,0 +1,41 @@
+import C from "./constants"
+import { v4 } from "uuid"
+
+export const addColor = (title, color) =>
+    ({
+        type: C.ADD_COLOR,
+        id: v4(),
+        title,
+        color,
+        rating: 0,
+        timestamp: new Date().toString()
+    })
+
+export const removeColor = id =>
+    ({
+        type: C.REMOVE_COLOR,
+        id
+    })
+
+export const rateColor = (id, rating) =>
+    ({
+        type: C.RATE_COLOR,
+        id,
+        rating
+    })
+
+export const sortColors = sortBy =>
+    (sortBy === "rating") ?
+        ({
+            type: C.SORT_COLORS,
+            sortBy: "SORT_BY_RATING"
+        }) :
+        (sortBy === "title") ?
+            ({
+                type: C.SORT_COLORS,
+                sortBy: "SORT_BY_TITLE"
+            }) :
+            ({
+                type: C.SORT_COLORS,
+                sortBy: "SORT_BY_DATE"
+            })
