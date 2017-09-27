a6b1a8907c33f31854f8bce44e4e2d62c63e0a12
TrungDT <thanhtrung.it25@gmail.com>
2017-09-27 11:26:40 +0700

    Dispach action removeColor from store

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const store = storeFactory()
 store.dispatch(addColor("Bonkers Blue", "#1122FF"))
 store.dispatch(rateColor("8658c1d0-9eda-4a90-95e1-8001e8eb6036", 5))
 store.dispatch(sortColors("title"))
+store.dispatch(removeColor("a5685c39-6bdc-4727-9188-6c9a00bf7f95"))
 
 console.log('current state', store.getState())
 console.log('Go ahead, dispatch some actions...')
