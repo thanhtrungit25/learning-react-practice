ee900574cd627a7f03b17027b0b3b7d6d0448300
TrungDT <thanhtrung.it25@gmail.com>
2017-09-27 12:03:52 +0700

    Exlicitly passing the store to App component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
 import storeFactory from './store'
 import { addColor, removeColor, rateColor, sortColors } from './actions'
 
 const store = storeFactory()
 
-store.dispatch(addColor("Bonkers Blue", "#1122FF"))
-store.dispatch(rateColor("8658c1d0-9eda-4a90-95e1-8001e8eb6036", 5))
-store.dispatch(sortColors("title"))
-store.dispatch(removeColor("a5685c39-6bdc-4727-9188-6c9a00bf7f95"))
+const render = () =>
+    ReactDOM.render(
+        <App store={store} />,
+        document.getElementById('react-container')
+    )
 
-console.log('current state', store.getState())
-console.log('Go ahead, dispatch some actions...')
-
-// window.React = React
-window.store = store
-window.addColor = addColor
-window.removeColor = removeColor
-window.rateColor = rateColor
-window.sortColors = sortColors
+store.subscribe(render)
+render()
