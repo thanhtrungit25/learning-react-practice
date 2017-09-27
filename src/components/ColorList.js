6a9718ac494c4449d18da0bdae98bec2798e0c75
TrungDT <thanhtrung.it25@gmail.com>
2017-09-27 13:10:59 +0700

    Refactor ColorList without dispatch action

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -4,38 +4,20 @@ import { rateColor, removeColor } from '../actions'
 import '../../stylesheets/ColorList.scss'
 import { sortFunction } from '../lib/array-helpers'
 
-const ColorList = ({ store }) => {
-    const { colors, sort } = store.getState()
-    const sortedColors = [...colors].sort(sortFunction(sort))
-
-    return (
-        <div className="color-list">
-            {(sortedColors.length === 0) ?
-                <p>No Colors Listed. (Add a Color)</p> :
-                sortedColors.map(color =>
-                    <Color key={color.id}
-                        {...color}
-                        onRate={(rating) =>
-                            store.dispatch(
-                                rateColor(color.id, rating)
-                            )
-                        }
-                        onRemove={() =>
-                            store.dispatch(
-                                removeColor(color.id)
-                            )
-                        }
-                    />
-                )
-            }
-        </div>
-    )
-}
+const ColorList = ({ colors=[] }) =>
+    <div className="color-list">
+        {(colors.length === 0) ?
+            <p>No Colors Listed. (Add a Color)</p> :
+            colors.map(color =>
+                <Color key={color.id}
+                    {...color}
+                />
+            )
+        }
+    </div>
 
 ColorList.propTypes = {
     colors: PropTypes.array,
-    onRate: PropTypes.func,
-    onRemove: PropTypes.func 
 }
 
 export default ColorList
