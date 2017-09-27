231d782ab0aee27d4aa65d982b7289506f8eecba
TrungDT <thanhtrung.it25@gmail.com>
2017-09-27 12:58:25 +0700

    Adding context store to App root component and add listener update

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,47 @@
-import { Component } from 'react'
+import { PropTypes, Component } from 'react'
 import { v4 } from 'uuid'
 import AddColorForm from './AddColorForm'
 import ColorList from './ColorList'
 import '../../stylesheets/APP.scss'
+import { sortFunction } from '../lib/array-helpers'
 
-const App = ({ store }) => {
-    <div className="app">
-        <SortMenu store={store} />
-        <AddColorForm store={store} />
-        <ColorList store={store} />
-    </div>
+class App extends Component {
+
+    getChildContext() {
+        return {
+            store: this.props.store
+        }
+    }
+
+    componentWillMount() {
+        store.subscribe(
+            () => this.forceUpdate()
+        )
+    }
+
+    componentWillUnmount() {
+        this.unsubscribe()
+    }
+
+    render() {
+        const { colors, sort } = store.getState()
+        const sortedColors = [...colors].sort(sortFunction(sort))
+        return (
+            <div className="app">
+                <SortMenu />
+                <AddColorForm />
+                <ColorList colors={sortedColors} />
+            </div>
+        )
+    }
+}
+
+App.propTypes = {
+    store: PropTypes.object.isRequired
+}
+
+App.childContextTypes = {
+    store: PropTypes.object.isRequired
 }
 
 export default App
